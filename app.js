082199139340c1871ca2dd26dc9994d1ae43bea4
Ivan <ziluhamacleod@gmail.com>
2017-04-28 14:07:42 +0300

    ll

---
+++ app.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+
+app.use('/', (req, res)=>{
+    res.end('Hello world');
+});
+app.listen(process.env.PORT||3030, () => console.log('Hello world'));
