c3f647e6c7dea2a9c4deb49cc9bda914467bd04b
Ivan <ziluhamacleod@gmail.com>
2017-04-28 14:35:39 +0300

    qwe

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use('/', (req, res)=>{
-    res.end('Hello world and zyabls');
+    res.end('Hello world and nodejs');
 });
 app.listen(process.env.PORT||3030, () => console.log('Hello world and zyabls'));
